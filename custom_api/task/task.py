50e9fa30195cb0758a5b3fca7247f29ddbd4857f
Saidi <mohannadsaidi@gmail.com>
2023-12-29 20:46:52 +0200

    Daily task email format

--- custom_api/task/task.py
+++ custom_api/task/task.py
@@ -9,17 +9,20 @@ def daily_notifications():
     users = frappe.db.get_list("User", fields=["*"], limit=0, order_by="modified desc")
     tasks_by_user = {}
     for user in users:
-        user_tasks = frappe.db.get_list("Task", fields=["name"], limit=0, filters=[["status","not in", ["Completed", "Cancelled"]],["Task User", "User", "in", user.name]])
+        user_tasks = frappe.db.get_list("Task", fields=["name","subject"], limit=0, filters=[["status","not in", ["Completed", "Cancelled"]],["Task User", "User", "in", user.name]])
         if user_tasks:
-           tasks_by_user[user.name] = user_tasks
+           tasks_by_user[user.name] = {}
+           tasks_by_user[user.name]['name'] = user.first_name
+           tasks_by_user[user.name]['tasks'] = user_tasks
     email_args = {}
     for user in tasks_by_user:
         subject = "{0} Pending Tasks".format(len(tasks_by_user[user]))
         counter = 1
-        user_email_summary = ""
-        for user_task in tasks_by_user[user]:
-            user_email_summary += "{0}- {1} \n".format(counter, user_task.name)
+        user_email_summary = f"<h4>Hello {tasks_by_user[user]['name']},</h4><h4>You have the following unfinished tasks:<ul>"
+        for user_task in tasks_by_user[user]['tasks']:
+            user_email_summary += f"<li><a href='{frappe.utils.get_url()}/Task/{user_task.name}' target='_blank'>{counter}- {user_task.subject}</a></li>"
             counter += 1
+        user_email_summary += "</ul>"
         email_args[user] = {
             "recipients": user,
             "sender": None,
