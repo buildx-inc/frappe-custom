410249e1f25f324fc7c5164126a469424e42e86e
saidi26 <mohannadsaidi@gmail.com>
2024-06-22 16:40:34 +0000

    added create attendance function

--- custom_api/hooks.py
+++ custom_api/hooks.py
@@ -6,3 +6,13 @@ app_publisher = "Buildx"
 app_description = "Custom Api collection for core Buildx logic"
 app_email = "info@buildx.ps"
 app_license = "MIT"
+
+
+doc_events = {
+    "Purchase Invoice":{
+        "before_submit": "custom_api.api.purchase_invoice_before_submit"
+    },
+    "Employee Checkin":{
+        "after_insert": "custom_api.api.create_employee_attendance"
+    }
+}
