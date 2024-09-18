6b37c3483ffdfefdcaa78435986c37ebce87c750
saidi26 <mohannadsaidi@gmail.com>
2024-09-18 20:28:11 +0000

    sync 2

---
+++ custom_api/custom_api/doctype/multiimages/multiimages.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Buildx and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class MultiImages(Document):
+	pass
