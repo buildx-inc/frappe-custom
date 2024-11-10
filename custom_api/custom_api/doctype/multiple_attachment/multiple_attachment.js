f18d9b81fb014ad5bf4e2252af5a8044af8b9178
saidi26 <mohannadsaidi@gmail.com>
2024-11-10 09:50:04 +0200

    added poll & multiple_attachment doctypes

---
+++ custom_api/custom_api/doctype/multiple_attachment/multiple_attachment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Buildx and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Multiple Attachment", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ custom_api/custom_api/doctype/multiple_attachment/test_multiple_attachment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Buildx and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMultipleAttachment(FrappeTestCase):
+	pass
