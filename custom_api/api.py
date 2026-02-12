cc556d41aa3ab246cb61d367ae6ed2b633e0e5ec
saidi26 <mohannadsaidi@gmail.com>
2026-02-12 09:13:36 +0000

    custom_api: update employee check-in logic to assign company from employee record, improving data integrity during attendance processing

--- custom_api/api.py
+++ custom_api/api.py
@@ -2412,10 +2412,9 @@ def create_doc_and_multi_payment(reference_doc,multi_payment,submit=False):
 
 
 def employee_checkin_on_update(doc, method):
+    doc.company = frappe.db.get_value("Employee", {'name': doc.employee}, "company")
     if doc.attendance != None and doc.attendance != "":
         attendance = frappe.get_doc("Attendance", doc.attendance)
         attendance.cancel()
         attendance.delete()
     create_employee_attendance(doc.company)
-
-    frappe.db.commit()
